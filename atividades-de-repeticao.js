a0a2ed9f71f7a4cf7408e6eb8d0d33c76e71e810
GabrielVieira-a <gabrielvaranha2006@gamil.com>
2025-06-30 11:56:49 -0300

    meu commit

---
+++ atividades-de-repeticao.js
@@ -0,0 +1,21 @@
+// for(let i =1; i<=10; i++){
+//     console.log(`5 x ${i} = `, i * 5)
+// }
+
+// let contador = 0;
+// for(let i =1; i<=100; i++){
+//     contador += i
+// }console.log(`A soma dos 100 primeiros números é: `+ contador);
+
+// for(let i =1; i<=50; i++){
+//     if(i%2==0){
+//         console.log(i+` É um número par.`);
+//     }
+// }
+
+// let nomes = ["Camila", "Edgar", "lais", "Frank"]
+
+// for(let i =0; i<=4; i++){
+//     console.log(`Seja bem-vindo, ${nomes[i]}`)
+// }
+
